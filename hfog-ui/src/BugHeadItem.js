1727e7818a3b06598cee540901edccaed6e7ce09
hinst <hinstance@yandex.ru>
2017-06-28 23:43:47 +0300

    Added bug sorting by number: ascending, descending

--- hfog-ui/src/BugHeadItem.js
+++ hfog-ui/src/BugHeadItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class BugHeadItem extends React.Component {
+class BugHeadItem extends React.PureComponent {
 
 	constructor(props) {
 		super(props);
--- hfog-ui/src/BugListPanel.js
+++ hfog-ui/src/BugListPanel.js
@@ -5,17 +5,42 @@ class BugListPanel extends React.Component {
 
 	constructor(props) {
 		super(props);
+		this.state = {
+      		sortAscending: false,			
+		}
+	}
+
+	getSortedBugs() {
+		const bugs = this.props.bugs.slice();
+		bugs.sort((a, b) => {
+			var diff = a.Number - b.Number;
+			if (false == this.state.sortAscending) {
+				diff = -diff;
+			}
+			return diff;
+		});
+		return bugs;
 	}
 
 	render() {
-		const bugHeadItems = this.props.bugs.map(
+		const bugs = this.getSortedBugs();
+		const bugHeadItems = bugs.map(
 			(bug) => {
 				return (<BugHeadItem key={bug.Number} number={bug.Number} title={bug.Title}></BugHeadItem>);
 			}
 		);
 		return (<div>
+			<div className="w3-panel">
+				<button className="w3-btn w3-black" onClick={() => this.changeSortDirection()}>{this.state.sortAscending ? "▲" : "▼"}</button>
+				<span style={{marginLeft: "4px"}}></span>
+				<button className="w3-btn w3-black">Refresh</button>
+			</div>
 			{bugHeadItems}
 		</div>);
+	}
+
+	changeSortDirection() {
+		this.setState({sortAscending: !this.state.sortAscending});
 	}
 
 }
