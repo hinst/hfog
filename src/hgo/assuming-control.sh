7a25ecf67eda81d604540281d47e1c1414d18cf9
hinst <hinstance@yandex.ru>
2017-07-02 16:16:12 +0300

    bash files

--- src/hgo/assuming-control.sh
+++ src/hgo/assuming-control.sh
@@ -2,15 +2,16 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 target="caddy"
+path=$(realpath $target)
 echo "$target control: $1"
+
 if [ "$1" = "start" ]
 then
-	nohup ./$target 2>>error-log.txt 1>>output-log.txt &
+	nohup $path 2>>error-log.txt 1>>output-log.txt &
 fi
 
 if [ "$1" == "stop" ]
 then
-	path=$(realpath $target)
 	pid=$(pidof $path)
 	if [[ $? == 0 ]]
 	then
